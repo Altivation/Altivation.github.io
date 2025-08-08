74f62d3f4c47ce38025bffe74dad788c504e0a31
Edward Cheng <flyingsky375@gmail.com>
2025-08-07 23:03:13 -0700

    testing

--- lifetracker/login-success.js
+++ lifetracker/login-success.js
@@ -3,9 +3,9 @@ window.onload = () => {
     console.log( "Local storage token:", localStorage.getItem('google_token') );
     const isLoggedIn = localStorage.getItem('google_token') !== null;
     if (isLoggedIn) {
-        window.location.href = getPath('/calendar.html');
+        //window.location.href = getPath('/calendar.html');
     }
     else {
-        window.location.href = getPath('/index.html');
+        //window.location.href = getPath('/index.html');
     }
 }
