7fc9137d9d4305e7bf84e49d89121a5cdf9cd0ea
Edward Cheng <flyingsky375@gmail.com>
2025-08-07 22:11:12 -0700

    debug script

--- lifetracker/login-success.js
+++ lifetracker/login-success.js
@@ -1,4 +1,6 @@
 window.onload = () => {
+    console.log( "Checking login status...");
+    console.log( "Local storage token:", localStorage.getItem('google_token') );
     const isLoggedIn = localStorage.getItem('google_token') !== null;
     if (isLoggedIn) {
         window.location.href = getPath('/calendar.html');
