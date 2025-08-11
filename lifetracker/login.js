7f75a46b693c42e7a4f485ec58113f3e59c48301
Edward Cheng <flyingsky375@gmail.com>
2025-08-11 02:20:38 -0700

    fix impoirt breakage

--- lifetracker/login.js
+++ lifetracker/login.js
@@ -1,8 +1,7 @@
 import { getPath } from './utility.js';
-import { getGoogleAuthURL } from './googleAuth.js';
+import { getGoogleAuthURL } from './googleAPI.js';
 
 const m_clientID = "534160900491-mkm54kduoibu8bvg0mqdqkhi38f53kq0.apps.googleusercontent.com"
-const m_redirectURI = "https://altivation.github.io/lifetracker/login-success.html";
 
 window.onload = () => {
     google.accounts.id.initialize({
@@ -18,10 +17,10 @@ function handleCredentialResponse(response) {
 }
 
 function triggerGoogleLogin() {
-
+    console.log('triggered');
     const url = getGoogleAuthURL();
     const popup = window.open(url, 'Google Login', 'width=400,height=600');
-
+    console.log( 'url', url);
     window.addEventListener('message', (event) => {
         if (event.origin !== window.location.origin) return;
         if (event.data.type === 'googleAuth') {
@@ -31,5 +30,5 @@ function triggerGoogleLogin() {
         }
         window.location.href = getPath('/calendar.html');
     });
-    
 }
+window.triggerGoogleLogin = triggerGoogleLogin;
