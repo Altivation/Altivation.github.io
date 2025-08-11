7443168766d28f9faacad1c49b2f1dc7359facb7
Edward Cheng <flyingsky375@gmail.com>
2025-08-11 09:33:12 -0700

    making them export

--- lifetracker/googleAPI.js
+++ lifetracker/googleAPI.js
@@ -1,7 +1,7 @@
 const m_clientID = "534160900491-mkm54kduoibu8bvg0mqdqkhi38f53kq0.apps.googleusercontent.com"
 const m_redirectURI = "https://altivation.github.io/lifetracker/login-success.html";
 
-function getGoogleAuthURL() {
+export function getGoogleAuthURL() {
     const url = `https://accounts.google.com/o/oauth2/v2/auth?` +
         `client_id=${m_clientID}&` +
         `redirect_uri=${encodeURIComponent(m_redirectURI)}&` +
@@ -10,4 +10,14 @@ function getGoogleAuthURL() {
         `prompt=consent`;
 
     return url;
+}
+
+export function loadGoogleApi() {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = "https://apis.google.com/js/api.js";
+        script.onload = resolve;
+        script.onerror = reject;
+        document.head.appendChild(script);
+    });
 }
